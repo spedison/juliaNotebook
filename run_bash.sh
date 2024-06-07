5704a1a083685ad0cfffb0bbe9e65ca668e7c421
EDISON RIBEIRO ARAUJO <spedison@users.noreply.github.com>
2024-06-07 11:40:17 -0300

    Update run_bash.sh

--- run_bash.sh
+++ run_bash.sh
@@ -1,1 +1,1 @@
-docker run -it --rm  -p 8888:8888 -v ${PWD}:/working  spedison/julia-notebook /bin/bash
+docker run -it --rm  -p 8888:8888 -v ${PWD}:/working -e PYDEVD_DISABLE_FILE_VALIDATION=1  spedison/julia-notebook /bin/bash
